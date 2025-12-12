dce57288bade69b6799a50dc0a2e7fb45805d188
qrzn23 <qrzn23@gmail.com>
2025-12-12 21:17:22 +0100

    chore: tile-based ground and camera rig in test room

---
+++ scripts/rooms/TestRoomGround.gd
@@ -0,0 +1,9 @@
+extends TileMap
+
+@export var width_tiles: int = 70
+@export var tile_source_id: int = 0
+@export var atlas_coords: Vector2i = Vector2i.ZERO
+
+func _ready() -> void:
+	for x in width_tiles:
+		set_cell(0, Vector2i(x, 0), tile_source_id, atlas_coords)
