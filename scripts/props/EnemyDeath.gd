6ee66f5d36d9cc9ca83a1d7d6fb3157dd5c4ce1c
qrzn23 <qrzn23@gmail.com>
2025-12-18 10:53:26 +0100

    fix: auto-free enemy death fx in headless state

--- scripts/props/EnemyDeath.gd
+++ scripts/props/EnemyDeath.gd
@@ -1,13 +1,19 @@
 extends Node2D
 
 @export var animation_name: StringName = &"default"
+@export var max_lifetime: float = 0.8
 
 func _ready() -> void:
     add_to_group("enemy_death_fx")
     var sprite := $AnimatedSprite2D as AnimatedSprite2D
     sprite.animation_finished.connect(_on_animation_finished)
     sprite.play(animation_name)
+    get_tree().create_timer(max_lifetime).timeout.connect(_on_lifetime_expired)
 
 func _on_animation_finished() -> void:
     queue_free()
 
+func _on_lifetime_expired() -> void:
+    if is_queued_for_deletion():
+        return
+    queue_free()
